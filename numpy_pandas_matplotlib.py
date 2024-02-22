fbee7a07b716d146c2ca5a08b916d5ec7582712c
siwookim <siwookim1125@gmail.com>
2024-02-22 17:57:52 +0900

    240220PracticeUpdate

---
+++ numpy_pandas_matplotlib.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+# --------------------------------------------------------------- pandas
+# The most basic form of pandas Dataframe
+values = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+index = ["one", "two", "three"]
+columns = ['A', 'B', 'C']
+
+df = pd.DataFrame(values, index = index, columns = columns)
+print (df)
+# --------------------------------------------------------------- calling methods
+print(df.head(2)) # front 2 row
+print (df.tail(1)) # end 1 row
+
+print (df['C']) # column
+# --------------------------------------------------------------- reading external files
+#c = pd.read_excel("pracsc.xlsx", sheet_name="Sheet1", engine="openpyxl")
+#print(c)
+# --------------------------------------------------------------- numpy
+
+
+# --------------------------------------------------------------- ML Workflow
+'''
+1) Acquisition (of data)
+2) Inspection and Exploration (EDA_ Exploratory Data Analysis)
+3) Preprocessing and Cleaning 
+4) Modelling and training
+5) Evaluation
+'''
+# --------------------------------------------------------------- Data Splitting
+'''
+Total Data: 20000
+X_train: 18000
+Y_train: 18000 (shown and learned)
+
+X_test: 2000
+Y_test: 2000 (unseen and tested)
+
+test result --> accuracy of the model
+'''
+# --------------------------------------------------------------- Data Splitting using zip method
+x, y = zip(['a',1],['b',2],['c',3])
+print (x) #tuple
+print (y) #tuple
+# --------------------------------------------------------------- Data Splitting using df
+
+values2 = [['abcdefg',1],
+           ['dsadosa',1],
+           ['dskaldj',0],
+           ['hghfhgf',1]
+           ]
